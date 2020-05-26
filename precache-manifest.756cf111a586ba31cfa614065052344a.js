6f41cb7b38d46d3666e24333c5b7b9b2f6b63c26
Nitin <er.nitinpatel914@gmail.com>
2020-05-26 00:26:07 -0500

    Updates

--- precache-manifest.756cf111a586ba31cfa614065052344a.js
+++ precache-manifest.756cf111a586ba31cfa614065052344a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff14bc0ffffa4abfaf49e0ad2403ad98",
+    "revision": "4ed0adb607bc7b2e132e7a3faf950ec7",
     "url": "/robofriends/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "bb312ffeef40d6734964",
-    "url": "/robofriends/static/css/main.f7bd701f.chunk.css"
+    "revision": "4c0024db5d40f14d3c93",
+    "url": "/robofriends/static/css/main.609f79d3.chunk.css"
   },
   {
     "revision": "670d2f135d86b771fc72",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/js/2.6f2abf64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb312ffeef40d6734964",
-    "url": "/robofriends/static/js/main.2c0ff3d7.chunk.js"
+    "revision": "4c0024db5d40f14d3c93",
+    "url": "/robofriends/static/js/main.52071ebf.chunk.js"
   },
   {
     "revision": "99557a09ab68caec6407",
